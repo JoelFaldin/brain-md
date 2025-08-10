7c2f9c30fccb97e0ed2501de5c2907afd0e2da24
JoelF <joelfaldin@gmail.com>
2025-08-10 00:17:52 -0400

    feat: adding note section on sidebar

--- src/components/EditorSidebar.tsx
+++ src/components/EditorSidebar.tsx
@@ -1,14 +1,21 @@
 import Logout from "../icons/Logout"
+import Note from "../icons/Note"
 import Plus from "../icons/Plus"
 import Search from "../icons/Search"
 
 const EditorSidebar = () => {
+  const notes = [
+    "Project ideas",
+    "CS Notes",
+    "Lunch",
+  ]
+
   return (
-    <aside className="w-80 bg-[var(--card)] flex flex-col gap-y-3">
+    <aside className="w-80 h-screen bg-[var(--card)] flex flex-col gap-y-3">
 
       <section className="flex flex-row gap-x-3 p-4 items-center border-b border-[var(--border)]">
-        <div className="bg-[var(--muted-foreground)] rounded-full w-5 h-5 p-2" />
-        <span>
+        <div className="bg-[var(--muted-foreground)] rounded-full w-8 h-8 p-2" />
+        <span className="flex-1">
           <div>John Doe</div>
           <div>johnd@example.com</div>
         </span>
@@ -16,20 +23,6 @@ const EditorSidebar = () => {
           <Logout className="h-5 w-5" />
         </div>
       </section>
-
-      {/* <section className="flex flex-col">
-        <span className="relative block mx-auto">
-          <Search className="absolute left-7 top-1/2 transform -translate-y-1/2 h-4 w-4 text-[var(--muted-foreground)]" />
-          <input
-            placeholder="Search notes..."
-            className="pl-10 bg-[var(--card)] border border-[var(--border)] rounded-md p-2 m-4"
-          />
-        </span>
-        <button className="bg-[var(--primary)] p-2 m-4 rounded-md flex flex-row items-center justify-center gap-x-3">
-          <Plus className="h-4 w-4 text-[var(--muted)]" />
-          <span className="text-[var(--muted)]">New Note</span>
-        </button>
-      </section> */}
 
       <section>
         <div className="p-4 space-y-3">
@@ -45,6 +38,21 @@ const EditorSidebar = () => {
             New Note
           </button>
         </div>
+      </section>
+
+      <section className="p-2 flex-1">
+        <span className="text-[var(--muted-foreground)] text-sm mb-4 block">Notes</span>
+        <ul className="flex flex-col gap-y-1 w-full">
+          {
+            notes.length > 0 ?notes.map((text) => (
+              <li key={`noteList${text}`} className="flex flex-row items-center justify-start gap-x-3 pl-5 p-2 rounded-md bg-transparent hover:bg-[var(--primary)]/80 border border-[var(--border)] cursor-pointer transition-colors">
+                <Note className="w-5 h-5" />
+                {text}
+              </li>
+            ))
+            : <li>No notes created (yet).</li>
+          }
+        </ul>
       </section>
 
     </aside>
